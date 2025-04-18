fe07fadd85d8659da5586d799f3ecd0f97e5ef06
a_zyada2010 <a_zyada2010@hotmail.com>
2025-04-19 00:56:16 +0200

    initiate angular app

---
+++ src/app/app.component.ts
@@ -0,0 +1,8 @@
+
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: '<h1>Chatbot Service Frontend is working 🚀</h1>',
+})
+export class AppComponent {}
